1ad91e33b48e07eef47effebe5e50a9b497ac68e
hemcodes28 <hemkumarr2803@gmail.com>
2026-07-11 13:27:24 +0530

    make base path dynamic for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/preset-sites/zenith-realty/',
+  base: process.env.VERCEL ? '/' : '/preset-sites/zenith-realty/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
