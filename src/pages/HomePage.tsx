623058836d66168c8e370bc8ec35a7e0428c6a8b
hemcodes28 <hemkumarr2803@gmail.com>
2026-08-01 15:02:19 +0530

    landing page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,6 @@
 import { HeroSection } from '../components/HeroSection';
 import { QuoteSection } from '../components/QuoteSection';
 import { ShowcaseSection } from '../components/ShowcaseSection';
-import { HowItWorksSection } from '../components/HowItWorksSection';
-import { InvestmentSection } from '../components/InvestmentSection';
-import { BlogSection } from '../components/BlogSection';
-import { LoanCalculatorSection } from '../components/LoanCalculatorSection';
-import { ContactSection } from '../components/ContactSection';
 
 export function HomePage() {
   return (
@@ -13,11 +8,6 @@ export function HomePage() {
       <HeroSection />
       <QuoteSection />
       <ShowcaseSection />
-      <HowItWorksSection />
-      <InvestmentSection />
-      <BlogSection />
-      <LoanCalculatorSection />
-      <ContactSection />
     </>
   );
 }
